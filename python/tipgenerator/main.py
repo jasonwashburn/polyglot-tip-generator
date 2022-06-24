997575d79ee9725d63f64ee2bbfcc4b62b68d3c7
Jason Washburn <wburn@wburn.net>
2022-06-24 22:07:51 +0000

    typo fix

--- python/tipgenerator/main.py
+++ python/tipgenerator/main.py
@@ -13,7 +13,7 @@ def split_bill(total_bill: Union[float, int], num_people: int) -> float:
 
 def main():
     print("Welcome to the tip calculator.")
-    total_bill = float(input("What was the total bil? $"))
+    total_bill = float(input("What was the total bill? $"))
     tip_percent = int(
         input("What percentage tip would you like to give? 10, 12, or 15? ")
     )
